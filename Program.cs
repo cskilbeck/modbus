ed213875862c3a6d88d889b9a021868f1107fb6f
chs <chs>
2019-11-06 22:34:15 +0000

    2nd commit

--- Program.cs
+++ Program.cs
@@ -1,44 +1,87 @@
-﻿using System;
+﻿//////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO.Ports;
 
+//////////////////////////////////////////////////////////////////////
+
 namespace modbus
 {
+    //////////////////////////////////////////////////////////////////////
+
     class Program
     {
-        static void checksum(byte[] message)
+        //////////////////////////////////////////////////////////////////////
+
+        enum mode
+        {
+            constant_current = 0,
+            constant_voltage = 1,
+            constant_resistance = 2,
+            constant_watts = 3
+        };
+
+        //////////////////////////////////////////////////////////////////////
+
+        enum load_switch
+        {
+            on,
+            off
+        };
+
+        //////////////////////////////////////////////////////////////////////
+
+        static bool validate_checksum(byte[] message)
         {
             int l = message.Length;
-            if(l < 3)
-            {
-                throw new ArgumentException($"message is only {l} long, too short to be checksummed (must be >= 3)");
-            }
-            ushort crc = 0xffff;
-            for (int i=0; i<l-2; ++i)
-            {
-                crc ^= message[i];
-                for(int j=0; j<8; ++j)
-                {
-                    ushort xor = 0;
-                    if((crc & 1) != 0)
-                    {
-                        xor = 0xa001;
-                    }
-                    crc = (ushort)((crc >> 1) ^ xor);
-                }
-            }
-            message[l - 1] = (byte)(crc & 0xff);
-            message[l - 2] = (byte)(crc >> 8);
+            return checksum.get(message, l - 2) == (ushort)((message[l - 1] & 0xff) | (message[l - 2] << 8));
         }
+
+        //////////////////////////////////////////////////////////////////////
+
+        static void set_load_switch(load_switch s)
+        {
+            byte[] message = { 0x01, 0x06, 0x01, 0x0E, 0x00, 0x01, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            if(s == load_switch.on)
+            {
+                message[10] = 1;
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////
+
+        static void set_mode(mode m)
+        {
+            int mode = (int)m;
+            byte[] message = { 0x01, 0x06, 0x01, 0x10, 0x00, 0x01, 0x04, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00 };
+            message[7] = (byte)(mode >> 24);
+            message[8] = (byte)(mode >> 16);
+            message[9] = (byte)(mode >> 8);
+            message[10] = (byte)(mode >> 0);
+        }
+
+        //////////////////////////////////////////////////////////////////////
+
+        static bool get_status(out int volts, out int amps)
+        {
+            byte[] message = { 0x01, 0x03, 0x03, 0x00, 0x00, 0x00, 0x8E, 0x45 };
+
+            volts = 0;
+            amps = 0;
+            return true;
+        }
+
+        //////////////////////////////////////////////////////////////////////
 
         static void Main(string[] args)
         {
-            //byte[] message = { 0x01, 0x06, 0x01, 0x16, 0x00, 0x01, 0x04, 0x00, 0x00, 0x07, 0xD0, 0x0C, 0x9D };
             byte[] message = { 0x01, 0x06, 0x01, 0x16, 0x00, 0x01, 0x04, 0x00, 0x00, 0x07, 0xD0, 0x00, 0x00 };
-            checksum(message);
+            checksum.set(message);
+            bool x = validate_checksum(message);
         }
     }
 }
---
+++ checksum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modbus
+{
+    public static class checksum
+    {
+        //////////////////////////////////////////////////////////////////////
+
+        public static ushort get(byte[] message, int length)
+        {
+            ushort crc = 0xffff;
+            for (int i = 0; i < length; ++i)
+            {
+                crc ^= message[i];
+                for (int j = 0; j < 8; ++j)
+                {
+                    ushort xor = 0;
+                    if ((crc & 1) != 0)
+                    {
+                        xor = 0xa001;
+                    }
+                    crc = (ushort)((crc >> 1) ^ xor);
+                }
+            }
+            return crc;
+        }
+
+        //////////////////////////////////////////////////////////////////////
+
+        public static void set(byte[] message)
+        {
+            int l = message.Length;
+            ushort crc = get(message, l - 2);
+            message[l - 1] = (byte)(crc & 0xff);
+            message[l - 2] = (byte)(crc >> 8);
+        }
+
+    }
+}
