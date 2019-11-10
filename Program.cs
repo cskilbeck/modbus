492a981030a5de03ad0c3af33a18c9301d429444
chs <chs>
2019-11-10 21:28:47 +0000

    add baud parameter

--- Program.cs
+++ Program.cs
@@ -24,10 +24,11 @@ namespace modbus
 
             CommandOption com_port = app.Option("-p|--com", "com port (string)", CommandOptionType.SingleValue);
             CommandOption address = app.Option("-a|--address", "device address (byte)", CommandOptionType.SingleValue);
+            CommandOption baud_rate = app.Option("-b|--baud", "baud rate", CommandOptionType.SingleValue);
 
             app.OnExecute(() =>
             {
-                if (!(com_port.HasValue() && address.HasValue()))
+                if (!(com_port.HasValue() && address.HasValue() && baud_rate.HasValue()))
                 {
                     app.ShowHelp();
                     return 0;
@@ -35,16 +36,26 @@ namespace modbus
                 byte device_address;
                 if (!byte.TryParse(address.Value(), out device_address))
                 {
+                    Console.Error.WriteLine($"Invalid device address: {address.Value()}");
+                    app.ShowHelp();
+                    return 0;
+                }
+                int actual_baud_rate;
+                if(!int.TryParse(baud_rate.Value(), out actual_baud_rate))
+                {
+                    Console.Error.WriteLine($"Invalid baud rate: {baud_rate.Value()}");
                     app.ShowHelp();
                     return 0;
                 }
                 kp184 device = new kp184();
-                if (!device.open(com_port.Value(), 115200))
+                if (!device.open(com_port.Value(), actual_baud_rate))
                 {
                     return 0;
                 }
+                Console.WriteLine($"COM Port: {com_port.Value()}");
+                Console.WriteLine($"BAUD Rate: {actual_baud_rate}");
+                Console.WriteLine($"Address: {device_address}");
                 device.address = device_address;
-                device.flush();
                 device.get_status();
                 //device.set_mode(kp184.load_mode.constant_current);
                 //device.set_load_switch(kp184.load_switch.on);
