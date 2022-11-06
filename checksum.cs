418cc1d0605b84cfa21d6b7514774d42f6e84b5a
pmendiuk <pmendiuk@comcast.net>
2022-11-06 13:42:13 -0800

    Update checksum.cs

--- checksum.cs
+++ checksum.cs
@@ -55,6 +55,8 @@ namespace KP184
                     crc = (ushort)((crc >> 1) ^ xor);
                 }
             }
+            // Flip upper and lower CRC bytes for Kunkin FW Date > 2020
+            crc = (ushort)((crc & 0xFFU) << 8 | (crc & 0xFF00U) >> 8);
             return crc;
         }
 
